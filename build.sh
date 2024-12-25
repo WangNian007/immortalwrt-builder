1109dc8d6c66f9d27dc75936d8093c5d4b9ed05f
rose <wangnian0925@outlook.com>
2024-12-25 21:40:30 +0800

    '个性化修改'

--- build.sh
+++ build.sh
@@ -26,37 +26,22 @@ PACKAGES="$PACKAGES luci-i18n-statistics-zh-cn"
 PACKAGES="$PACKAGES luci-app-openclash"
 # Passwall 代理
 PACKAGES="$PACKAGES luci-i18n-passwall-zh-cn"
-# frp 和 nps 内网穿透
-PACKAGES="$PACKAGES luci-i18n-nps-zh-cn luci-i18n-frpc-zh-cn"
 
 # 常用的网络存储组件
 
+#SmartDns
+PACKAGES="$PACKAGES luci-app-smartdns luci-i18n-smartdns-zh-cn"
+
 # 文件助手
 PACKAGES="$PACKAGES luci-app-fileassistant"
-# 硬盘休眠
-PACKAGES="$PACKAGES luci-i18n-hd-idle-zh-cn"
-# p910nd 打印服务器
-PACKAGES="$PACKAGES luci-i18n-p910nd-zh-cn"
-# GoWebDav
-#PACKAGES="$PACKAGES luci-i18n-gowebdav-zh-cn"
-# Samba 网络共享
-PACKAGES="$PACKAGES luci-i18n-samba4-zh-cn"
-# qBittorrent 下载器
-PACKAGES="$PACKAGES luci-i18n-qbittorrent-zh-cn"
-# Aria2 下载器
-PACKAGES="$PACKAGES luci-i18n-aria2-zh-cn"
-# FTP 服务器
-PACKAGES="$PACKAGES luci-i18n-vsftpd-zh-cn"
 
 # Docker 组件
 PACKAGES="$PACKAGES luci-i18n-dockerman-zh-cn luci-i18n-docker-zh-cn"
 
 # VPN 类
 
-# IPSec VPN
-PACKAGES="$PACKAGES luci-i18n-ipsec-vpnd-zh-cn"
-# Zerotier
-PACKAGES="$PACKAGES luci-i18n-zerotier-zh-cn"
+#wireguard
+PACKAGES="$PACKAGES luci-proto-wireguard"
 
 # 网络组件
 
@@ -72,9 +57,6 @@ PACKAGES="$PACKAGES luci-i18n-opkg-zh-cn luci-i18n-base-zh-cn luci-i18n-firewall
 
 # Packages 文件夹下的 ipk 包
 PACKAGES="$PACKAGES luci-i18n-wrtbwmon-zh-cn"
-
-# 一些自己需要的内核组件
-PACKAGES="$PACKAGES kmod-usb-printer kmod-lp"
 
 # 一些其他可能有用的包
 
