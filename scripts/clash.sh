6fd97b5b2fb1c291f29f524aa17b26513c757844
WangNian007 <116576935+WangNian007@users.noreply.github.com>
2025-07-14 22:20:54 +0800

    更新openclash内核

--- scripts/clash.sh
+++ scripts/clash.sh
@@ -6,24 +6,23 @@ echo "Current Path: $PWD"
 mkdir -p files/etc/openclash/core
 cd files/etc/openclash/core || (echo "Clash core path does not exist! " && exit)
 
-# Clash Dev
-wget https://raw.githubusercontent.com/vernesong/OpenClash/core/master/dev/clash-linux-amd64.tar.gz
-tar -zxvf clash-linux-amd64.tar.gz
-rm -rf clash-linux-amd64.tar.gz
-mv clash clash_dev
+# Clash Dev 已停止更新
+#wget https://raw.githubusercontent.com/vernesong/OpenClash/core/master/dev/clash-linux-amd64.tar.gz
+#tar -zxvf clash-linux-amd64.tar.gz
+#rm -rf clash-linux-amd64.tar.gz
+#mv clash clash_dev
 
-# Clash TUN
-VERSION=$(curl -sS https://raw.githubusercontent.com/vernesong/OpenClash/core/dev/core_version | awk 'NR==2')
-wget https://raw.githubusercontent.com/vernesong/OpenClash/core/master/premium/clash-linux-amd64-$VERSION.gz
-gzip -d clash-linux-amd64-$VERSION.gz
-rm -rf clash-linux-amd64-$VERSION.gz
-mv clash-linux-amd64-$VERSION clash_tun
+# Clash TUN 已停止更新
+#VERSION=$(curl -sS https://raw.githubusercontent.com/vernesong/OpenClash/core/dev/core_version | awk 'NR==2')
+#wget https://raw.githubusercontent.com/vernesong/OpenClash/core/master/premium/clash-linux-amd64-$VERSION.gz
+#gzip -d clash-linux-amd64-$VERSION.gz
+#rm -rf clash-linux-amd64-$VERSION.gz
+#mv clash-linux-amd64-$VERSION clash_tun
 
 # Clash Meta
-wget https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-amd64.tar.gz
-tar -zxvf clash-linux-amd64.tar.gz
-rm -rf clash-linux-amd64.tar.gz
-mv clash clash_meta
+wget https://github.com/MetaCubeX/mihomo/releases/download/v1.19.11/mihomo-linux-amd64-v1.19.11.gz
+gunzip mihomo-linux-amd64-v1.19.11.gz
+mv mihomo-linux-amd64-v1.19.11 clash_meta
 
-# Use clash_dev as default core
-mv clash_dev clash
+# Use clash_meta as default core
+mv clash_meta clash
