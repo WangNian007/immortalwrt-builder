9744a4553565f48646af6466ed677605d5966c1d
wangnian <wangnian1996@gmail.com>
2026-04-16 22:23:30 +0800

    添加mihomo内核

--- scripts/mihomo.sh
+++ scripts/mihomo.sh
@@ -7,7 +7,6 @@ cd $GITHUB_WORKSPACE/$VENDOR-imagebuilder-$VERSION-x86-64.Linux-x86_64 || exit
 
 
 # 下载 mihomo 二进制文件
-
 url=$(curl -s https://api.github.com/repos/MetaCubeX/mihomo/releases/latest | jq -r '.assets[] | select(.name | test("mihomo-linux-amd64-v2-go123")) | .browser_download_url') 
 
 wget "${url}"
@@ -16,15 +15,7 @@ wget "${url}"
 gunzip mihomo-linux-amd64-v2-*.gz
 
 
-#openclash 内核
-mkdir -p files/etc/openclash/core
-cp mihomo-linux-amd64-v2-* files/etc/openclash/core/clash_mate
-chmod 777 files/etc/openclash/core/clash_mate
-
-#nikki 内核
-mkdir -p files/usr/bin
-cp mihomo-linux-amd64-v2-* files/usr/bin/mihomo
-chmod 777 files/usr/bin/mihomo
-
+#文件写入临时目录
+cp mihomo-linux-amd64-v2-* files/tmp/
 
 exit 0
